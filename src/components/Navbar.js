bd0ab19d1d728213777c4c5bc9c92b677770f358
DanieleDeBa <danieledebattista@Air-di-Daniele.home>
2024-08-22 15:25:25 +0200

    modifiche

---
+++ src/components/Navbar.js
@@ -0,0 +1,49 @@
+import { useRef } from "react";
+import { FaBars, FaTimes } from "react-icons/fa";
+import "./Navbar.css";
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import texts from '../texts/texts.json';
+import Logo from './Logo';
+
+
+
+function Navbar() {
+	const navRef = useRef();
+
+	const showNavbar = () => {
+		navRef.current.classList.toggle(
+			"responsive_nav"
+		);
+	};
+
+	return (
+		<header>
+            {/* <div className="border">
+            </div> */}
+			<nav ref={navRef}>
+				<Link to="/react-test">HOME</Link>
+				<Link to="/About_me">CHI SONO</Link>
+				<a href="/#">IL MIO LAVORO</a>
+				<a href="/#">CONTATTI</a>
+				<button
+					className="nav-btn nav-close-btn"
+					onClick={showNavbar}>
+					<FaTimes />
+				</button>
+			</nav>
+            {/* <div className="">
+                <Logo/>
+            </div> */}
+            
+    
+			<button
+				className=" nav-btn nav-btn--burger"
+				onClick={showNavbar}>
+				<FaBars />
+			</button>
+		</header>
+	);
+}
+
+export default Navbar;
+
---
+++ src/pages/Aboutme.js
@@ -0,0 +1,10 @@
+function Aboutme() {
+    return (
+        <div className="hero">
+           <p>chi sono</p>
+        </div>
+     
+    );
+  }
+  
+  export default Aboutme;
